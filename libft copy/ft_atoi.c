0101902162a17da99f9cf555eb0375eede5f9a63
lordkallad <lordakllad@gmail.com>
2022-11-03 13:40:40 +0100

    FH

--- libft copy/ft_atoi.c
+++ libft copy/ft_atoi.c
@@ -20,7 +20,7 @@ int ft_atoi(const char *str)
     i = 0;
     num = 0;
     neg = 1;
-    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '+')
         i++;
     if (str[i] == '-')
     {
